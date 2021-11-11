febecfa5a72e608145766ad30a23696ecc6a353f
xiaosuGW <xiaosuGW@localhost>
2021-11-11 07:44:19 -0500

    upload test json and .js files

---
+++ sample/sledge-w4-65.js
@@ -0,0 +1,63 @@
+const loadtest = require("../lib/loadtest");
+const configuration = require('../sample/sledge_w4_65.json');
+const execSync = require('child_process').execSync;
+
+function statusCallback(error, result, latency) {
+    console.log('----');
+    console.log('Current latency %j, result %j, error %j', latency);
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    console.log('Request index: ', result.requestIndex);
+    console.log('Request loadtest() instance index: ', result.instanceIndex);
+}
+
+rps = configuration.rps;
+rpsInterval = configuration.interval;
+
+index = 0;
+
+const fs = require('fs');
+const path = require('path');
+function readBody(filename, option) {
+        if (typeof filename !== 'string') {
+                console.error('Invalid file to open with %s: %s', option, filename);
+                help();
+        }
+
+        if (path.extname(filename) === '.js') {
+                return require(path.resolve(filename));
+        }
+
+	const ret = fs.readFileSync(filename);
+        return ret;
+}
+
+const IH = '10.10.1.1';
+const IP = '10009';
+console.log(IH+':'+ IP);
+
+const options = {
+    url: 'http://' + IH + ':' + IP,
+    maxRequests: rps.reduce((a, b) => a + b, 0)*rpsInterval,
+    // maxRequests: 10000,
+    //headers: { 'Host': 'autoscale-go-1.default.example.com' },
+    //method: 'POST',
+    body: readBody("/users/xiaosuGW/sledge-serverless-framework/runtime/tests/40k.jpg",'-p'),
+    contentType: 'image/jpg',
+    // starting rps can be an array or a single value
+    requestsPerSecond: rps, //[20, 20, 20, 20],
+    concurrency: 1,
+    debug:true,
+    /**
+     * GWU:Custom parameters
+     */
+    rpsInterval,
+    agentKeepAlive: false,
+};
+
+loadtest.loadTest(options, function (error, result) {
+    if (error) {
+        return console.error('Got an error: %s', error);
+    }
+    console.log(result);
+    console.log('Tests run successfully');
+});
--- start_test.sh
+++ start_test.sh
@@ -3,12 +3,53 @@ chmod 400 $path/id_rsa
 
 
 #test single c10
-server_log_file="srsf_match.log"
+server_log_file="srsf_60.log"
 #vmstat_file="30cpucore_single1_vmstat.txt"
 #pidstat_file="30cpucore_single1_pidstat.txt"
 ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start.sh $server_log_file >/dev/null 2>&1 &"
 #ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start_monitor.sh $vmstat_file $pidstat_file"
 ./test.sh
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/kill_sledge.sh"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/stop_monitor.sh"
+
+############# 65 ############
+server_log_file="srsf_65.log"
+#vmstat_file="30cpucore_single1_vmstat.txt"
+#pidstat_file="30cpucore_single1_pidstat.txt"
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start.sh $server_log_file >/dev/null 2>&1 &"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start_monitor.sh $vmstat_file $pidstat_file"
+./test_65.sh
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/kill_sledge.sh"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/stop_monitor.sh"
+
+############# 70 ############
+server_log_file="srsf_70.log"
+#vmstat_file="30cpucore_single1_vmstat.txt"
+#pidstat_file="30cpucore_single1_pidstat.txt"
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start.sh $server_log_file >/dev/null 2>&1 &"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start_monitor.sh $vmstat_file $pidstat_file"
+./test_70.sh
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/kill_sledge.sh"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/stop_monitor.sh"
+
+
+############# 75 ############
+server_log_file="srsf_75.log"
+#vmstat_file="30cpucore_single1_vmstat.txt"
+#pidstat_file="30cpucore_single1_pidstat.txt"
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start.sh $server_log_file >/dev/null 2>&1 &"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start_monitor.sh $vmstat_file $pidstat_file"
+./test_75.sh
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/kill_sledge.sh"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/stop_monitor.sh"
+
+############# 80 ############
+server_log_file="srsf_80.log"
+#vmstat_file="30cpucore_single1_vmstat.txt"
+#pidstat_file="30cpucore_single1_pidstat.txt"
+ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start.sh $server_log_file >/dev/null 2>&1 &"
+#ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/start_monitor.sh $vmstat_file $pidstat_file"
+./test_80.sh
 ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/kill_sledge.sh"
 #ssh -o stricthostkeychecking=no -i $path/id_rsa xiaosuGW@10.10.1.1 "$path/stop_monitor.sh"
 
