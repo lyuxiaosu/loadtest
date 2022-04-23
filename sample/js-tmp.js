992451a5bac1ce99e826091a097444f116ce746e
xiaosuGW <xiaosuGW@localhost>
2022-04-23 11:09:42 -0400

    upload w1_tmp.json js-tmp.js

---
+++ sample/js-tmp.js
@@ -0,0 +1,64 @@
+const loadtest = require("../lib/loadtest");
+const configuration = require('../sample/w1_tmp.json');
+const execSync = require('child_process').execSync;
+
+function statusCallback(error, result, latency) {
+    console.log('----');
+    console.log('Current latency %j, result %j, error %j', latency);
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    console.log('Request index: ', result.requestIndex);
+    console.log('Request loadtest() instance index: ', result.instanceIndex);
+}
+
+rps = configuration.rps;
+rpsInterval = configuration.interval;
+
+index = 0;
+
+const fs = require('fs');
+const path = require('path');
+function readBody(filename, option) {
+        if (typeof filename !== 'string') {
+                console.error('Invalid file to open with %s: %s', option, filename);
+                help();
+        }
+
+        if (path.extname(filename) === '.js') {
+                return require(path.resolve(filename));
+        }
+
+	const ret = fs.readFileSync(filename);
+        return ret;
+}
+
+const IH = '10.10.1.1';
+const IP = '10000';
+console.log(IH+':'+ IP);
+
+const options = {
+    url: 'http://' + IH + ':' + IP,
+    //maxRequests: rps.reduce((a, b) => a + b, 0)*rpsInterval,
+    maxRequests: rps.reduce((a, b) => a + b, 0),
+    // maxRequests: 10000,
+    headers: { 'Host': 'autoscale-go-1.default.example.com' },
+    //method: 'POST',
+    //body: readBody("/users/xiaosuGW/sledge-serverless-framework/runtime/tests/105k.jpg",'-p'),
+    //contentType: 'image/jpg',
+    // starting rps can be an array or a single value
+    requestsPerSecond: rps,
+    concurrency: 1,
+    debug:true,
+    /**
+     * GWU:Custom parameters
+     */
+    rpsInterval,
+    agentKeepAlive: false,
+};
+
+loadtest.loadTest(options, function (error, result) {
+    if (error) {
+        return console.error('Got an error: %s', error);
+    }
+    console.log(result);
+    console.log('Tests run successfully');
+});
